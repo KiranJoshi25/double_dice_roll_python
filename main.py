794e332472801f1b39b69dda88fe647a6bb2c984
Kiran Joshi <joshikiran2537@gmail.com>
2021-05-13 01:03:52 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+
+
+import tkinter
 from random import randint
+import tkinter.messagebox
+
+top = tkinter.Tk()
+
 def roll_a_dice():
     j = randint(1,6)
     k = randint(1,6)
-    return j,k
+    tkinter.messagebox.showinfo("dice rolled", (j,k))
 
-roll1, roll2 = roll_a_dice()
-print(roll1,roll2)
+B1 = tkinter.Button(top, text = "roll a dice", command = roll_a_dice)
+B1.pack()
+top.mainloop()
+
+
+
+
+
+
+
